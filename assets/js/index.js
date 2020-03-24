6b9888b8e423ce103e00f348ab164f4cbf9e5308
cheng-long <1760016317@qq.com>
2020-03-24 14:11:17 +0800

    获取用户信息完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,7 +13,7 @@ $(function () {
         }, function () {
 
         });
-       
+
     });
     getUaerinfo();
 
@@ -26,10 +26,35 @@ function getUaerinfo() {
         url: '/my/userinfo',
         success: function (res) {
             console.log(res);
+            //调用函数
+            renderUser(res.data)
         },
         //配置请求头
         headers: {
             Authorization: localStorage.getItem('token')
         }
     });
+}
+//定义函数
+function renderUser(data) {
+    // 获取data里面的用户名和昵称
+    //只获取一个 如果第一个没有用怎获取第二个
+    var name = data.nickname || data.username;
+    //截取用户名第一个字母（转换成大写字母）或昵称的第一个字
+    var fontHead = name.substr(0, 1).toUpperCase();
+    // 判断是否有图片
+    if (data.user_pic) {
+        //如果有则执行
+        $('.head-pic img').show().attr('src', data.user_pic);
+        $('.head-pic .text-head-portrait').hide();
+    } else {
+        //如果没有图片，执行文字头像
+        //显示文字头像
+        $('.head-pic .text-head-portrait').css('display', 'inline-block').text(fontHead);
+        //隐藏图片头像
+        $('.head-pic img').hide()
+    }
+    //把用户名或昵称添加到页面中(注意只获取一个，如果有昵称就用昵称如果没有就用用户名)
+    $('.welcome').html('欢迎&nbsp;&nbsp;' + name);
+
 }
